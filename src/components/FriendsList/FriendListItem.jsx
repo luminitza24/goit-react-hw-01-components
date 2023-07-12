e64f33c074c8b9fdcb6e1666bf268491f8186ec1
luminitza24 <cluminita24@yahoo.com>
2023-07-12 06:51:05 +0300

    adaugat proiect

---
+++ src/components/FriendsList/FriendListItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{ background: isOnline ? 'blue' : 'red' }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt={name} />
+      <span className={css.name}>{name}</span>
+    </li>
+  );
+};
+FriendListItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
